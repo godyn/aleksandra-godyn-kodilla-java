dc4606a7e6b249ac1784106d1ef149394dc393c2
godyn <godyn.ola@gmail.com>
2019-01-17 21:01:31 +0100

    Task 6.2. Calculator class and unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calc = new Calculator();
+        int result1 = calc.add(5,6);
+        int result2 = calc.subtract(17,2);
+
+        if (result1 == 11){
+            System.out.println("Adding in Calculator works.");
+        } else{
+            System.out.println("Adding in Calculator - Error!");
+        }
+        if (result2 == 15){
+            System.out.println("Substract in Calculator works.");
+        } else{
+            System.out.println("Substract in Calculator - Error!");
+        }
+
     }
 }
