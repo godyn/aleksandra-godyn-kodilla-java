3d226a9d7910512a3a8ddda8514cf6b39f3a52d7
godyn <godyn.ola@gmail.com>
2019-01-25 11:52:29 +0100

    Task 7.1 - PoemBeautifier - Lambda

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+import static com.sun.tools.javac.util.StringUtils.toUpperCase;
+import static jdk.nashorn.internal.objects.NativeString.substring;
+
 public class StreamMain {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to module 7 - Stream");
+
+            PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+            String name = "Anna";
+
+            poemBeautifier.beautify(name, (text) -> "ABC " + text + " ABC");
+            poemBeautifier.beautify(name, (text) -> "*** " + text + " ***");
+            poemBeautifier.beautify(name, (text) -> toUpperCase(text));
+            poemBeautifier.beautify(name, (text) -> text.substring(0,1)+ " " +text.substring(1,2)+ " " +text.substring(2,3)+ " " +text.substring(3,4));
+        }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,15 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+
+    public PoemBeautifier() {
+    }
+
+    public void beautify (String text, PoemDecorator poemDecorator){
+
+        String effect = poemDecorator.decorate(text);
+
+        System.out.println(effect);
+
+    }
+}
